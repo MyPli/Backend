7d80d0bd813c7aac40776baff19e51fec7deea2a
Algoruu <zlfkcmspdbdp@naver.com>
2024-12-13 00:56:30 +0900

    [Fix] Playlist Entity에 태그 필드 추가 및 관계 설정 수정

---
+++ src/playlist/entities/playlist.entity.ts
@@ -0,0 +1,13 @@
+export class PlaylistEntity {
+    id: number;
+    userId: number;
+    title: string;
+    description?: string;
+    coverImage?: string;
+    likesCount: number;
+    createdAt: Date;
+    updatedAt: Date;
+    tags?: string[];
+    videos?: any[];
+  }
+  
