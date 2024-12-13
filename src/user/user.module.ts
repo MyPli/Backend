99f9e8dff25de69f8c703dbe403b0ea5d0b47bbb
Ss0Mae <ssomae@protonmail.ch>
2024-12-14 00:59:05 +0900

    [Feat]: user Module 구현

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
+import { Module, forwardRef } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { S3Module } from '../s3/s3.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    forwardRef(() => AuthModule), // forwardRef로 AuthModule 참조
+    PrismaModule,
+    S3Module,
+  ],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService], // UserService를 내보냄
 })
 export class UserModule {}
