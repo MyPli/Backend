d79049b4e272279fdb09f11e82c9ed916f8bbd58
Ss0Mae <ssomae@protonmail.ch>
2024-12-13 23:39:42 +0900

    [Feat]: 회원가입 DTO 구현

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,16 +1,15 @@
 import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
 
 export class SignupDto {
-  @IsEmail()
+  @IsEmail({}, { message: '올바른 이메일 형식이어야 합니다.' })
   email: string;
 
-  @IsString()
-  @MinLength(6)
-  @MaxLength(20)
+  @IsString({ message: '비밀번호는 문자열이어야 합니다.' })
+  @MinLength(6, { message: '비밀번호는 최소 6자 이상이어야 합니다.' })
   password: string;
 
-  @IsString()
-  @MinLength(2)
-  @MaxLength(20)
+  @IsString({ message: '닉네임은 문자열이어야 합니다.' })
+  @MinLength(2, { message: '닉네임은 최소 2자 이상이어야 합니다.' })
+  @MaxLength(20, { message: '닉네임은 최대 20자 이하이어야 합니다.' })
   nickname: string;
 }
