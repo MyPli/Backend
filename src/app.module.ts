2b07c006bd8cb1f1394cc668c34078cbf42bab16
Ss0Mae <ssomae@protonmail.ch>
2024-12-14 01:01:21 +0900

    [Feat]: 메인 모듈 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module'; // Prisma 연결
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule, // Auth 모듈 등록
+    UserModule, // User 모듈 등록
+    PrismaModule, // Prisma 모듈 등록
+  ],
 })
 export class AppModule {}
