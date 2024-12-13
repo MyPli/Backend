3918bff67b277b1590fc21603eb7398a988e0b19
Ss0Mae <ssomae@protonmail.ch>
2024-12-14 00:59:49 +0900

    [Feat]: main.ts 수정

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Swagger 설정 추가
+  // 전역 Validation Pipe 설정 (DTO 유효성 검사 활성화)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 없는 값은 자동으로 제거
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 값이 들어오면 에러 발생
+      transform: true, // 요청 데이터를 DTO로 자동 변환
+    }),
+  );
+
+  // Swagger 설정
   const config = new DocumentBuilder()
     .setTitle('MyPli API')
     .setDescription('플레이리스트 공유 서비스를 위한 REST API 문서입니다.')
     .setVersion('1.0')
-    .addTag('Playlist Service')
+    .addBearerAuth() // JWT 인증 추가
     .build();
 
-  // Swagger 문서 생성 및 설정
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); // '/api' 경로에서 Swagger UI 확인 가능
+  SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT);
+  // 서버 실행
+  const port = process.env.PORT;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
