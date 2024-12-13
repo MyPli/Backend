dba59a3876fa1d90dea8f46e0e35ddbab2c49d46
Ss0Mae <ssomae@protonmail.ch>
2024-12-14 01:41:45 +0900

    [Feat]: cors 설정 구현

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   // ValidationPipe 전역 설정 추가
   app.useGlobalPipes(
     new ValidationPipe({
