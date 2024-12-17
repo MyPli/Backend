e5ebf955f703503f7eda45da9c85374afc668216
Ss0Mae <ssomae@protonmail.ch>
2024-12-17 22:57:46 +0900

    [Comment]: 유저 프로필 사진 업데이트 DTO Swagger 주석 추가

--- src/user/dto/update-profile.dto.ts
+++ src/user/dto/update-profile.dto.ts
@@ -1,11 +1,20 @@
 import { IsOptional, IsString } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdateProfileDto {
+   @ApiPropertyOptional({
+    example: 'new_nickname',
+    description: '사용자의 새로운 닉네임',
+  })
   @IsOptional()
   @IsString()
   nickname?: string;
 
+  @ApiPropertyOptional({
+    example: 'new_profile_image.png',
+    description: '사용자의 새로운 프로필 이미지 파일',
+  })
   @IsOptional()
   @IsString()
-  profileImage?: string; // Base64로 전달
+  profileImage?: string; 
 }
