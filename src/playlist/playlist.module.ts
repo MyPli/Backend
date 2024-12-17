8aa0d50e8994acc8147f7ba058fc4840f41080b0
Algoruu <zlfkcmspdbdp@naver.com>
2024-12-17 23:43:46 +0900

    [Chore] VideoSerive 모듈 불러오기 수정

--- src/playlist/playlist.module.ts
+++ src/playlist/playlist.module.ts
@@ -3,10 +3,11 @@ import { PlaylistController } from './playlist.controller';
 import { PlaylistService } from './playlist.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
+import { VideoService } from '../video/video.service';
 
 @Module({
   imports: [PrismaModule, AuthModule],
   controllers: [PlaylistController],
-  providers: [PlaylistService],
+  providers: [PlaylistService, VideoService],
 })
 export class PlaylistModule {}
