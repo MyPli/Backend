7ff0b37176986c952ce21b6ca0dfd55dbd76dfab
Algoruu <zlfkcmspdbdp@naver.com>
2024-12-18 16:22:47 +0900

    [Feat] DTO Swagger 명세 추가

--- src/playlist/dto/update-playlist.dto.ts
+++ src/playlist/dto/update-playlist.dto.ts
@@ -1,17 +1,33 @@
 import { IsString, IsOptional, IsArray, ArrayNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdatePlaylistDto {
+  @ApiProperty({
+    example: 'Updated Playlist Title',
+    description: '플레이리스트 제목',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   title?: string;
 
+  @ApiProperty({
+    example: '업데이트된 플레이리스트 설명',
+    description: '플레이리스트 설명',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty({
+    example: ['업데이트', '새로운 태그'],
+    description: '플레이리스트 태그 목록',
+    required: false,
+  })
   @IsArray()
   @ArrayNotEmpty()
   @IsString({ each: true })
   @IsOptional()
-  tags?: string[]; // 태그는 문자열 배열로 입력받음
+  tags?: string[];
 }
