4d9b5d8d53ef0c14a9385e5f6e17e3ad687f7b34
Algoruu <zlfkcmspdbdp@naver.com>
2024-12-18 16:20:09 +0900

    [Feat] DTO Swagger 명세 추가

--- src/playlist/dto/create-playlist.dto.ts
+++ src/playlist/dto/create-playlist.dto.ts
@@ -1,13 +1,28 @@
 import { IsString, IsOptional, IsArray } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePlaylistDto {
+  @ApiProperty({
+    example: 'My Favorite Songs',
+    description: '플레이리스트 제목',
+  })
   @IsString()
   title: string;
 
+  @ApiProperty({
+    example: '즐겨듣는 노래들을 모았습니다.',
+    description: '플레이리스트 설명',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty({
+    example: ['Pop', 'K-Pop'],
+    description: '플레이리스트 태그 목록',
+    required: false,
+  })
   @IsArray()
   @IsString({ each: true })
   @IsOptional()
