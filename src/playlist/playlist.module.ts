3c40d29874576e2cb2648e2e5f849e5fd2886da0
Algoruu <zlfkcmspdbdp@naver.com>
2024-12-21 17:46:37 +0900

    [Feat] 플레이리스트 모듈에 비디오 모듈 의존성 추가

--- src/playlist/playlist.module.ts
+++ src/playlist/playlist.module.ts
@@ -3,10 +3,11 @@ import { PlaylistController } from './playlist.controller';
 import { PlaylistService } from './playlist.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
+import { VideoModule } from '../video/video.module';
 import { VideoService } from '../video/video.service';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, VideoModule],
   controllers: [PlaylistController],
   providers: [PlaylistService, VideoService],
 })
